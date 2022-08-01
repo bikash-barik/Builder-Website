5a340f6ea848efcf03e854a3144dff4c25c4036b
Purbesh Swain <purbeshswain5@gmail.com>
2022-08-02 01:11:46 +0530

    home page modified

--- src/pages/Landing/Banner/minicomponent/SearchFilter/index.js
+++ src/pages/Landing/Banner/minicomponent/SearchFilter/index.js
@@ -10,41 +10,41 @@ const SearchFilter = () => {
   const [priceRange, setPriceRange] = useState("ANY");
   return (
     <Container className="banner-search-filter ">
-    <Row  >
-    <Col xs={6} sm={3} >
-    <Filter
-    title="PROPERTY"
-    options={["Residential", "Comercial"]}
-    state={property}
-    setState={setProperty}
-  />
-    </Col>
-    <Col xs={6} sm={3}>
-    <Filter
-    title="BEDROOM"
-    options={["2 BHK", "3 BHK", "4 BHK"]}
-    state={bedroom}
-    setState={setBedroom}
-  />
-    </Col>
-    <Col xs={6} sm={3}>
-    <Filter
-        title="PRICE RANGE"
-        options={["60 - 80 Lakhs", "75 Lakhs - 2 Crore", "2 Crore & Above"]}
-        state={priceRange}
-        setState={setPriceRange}
-      />
-    </Col>
-    <Col xs={6} sm={3}>
-     
-    <div className="banner-search">
-    <div className="banner-search-button">SEARCH PROPERTIES</div>
-  </div>
-    </Col>
-    </Row>
-     
-    
-     
+      <Row  >
+        <Col xs={6} sm={3} >
+          <Filter
+            title="PROPERTY"
+            options={["Residential", "Comercial"]}
+            state={property}
+            setState={setProperty}
+          />
+        </Col>
+        <Col xs={6} sm={3}>
+          <Filter
+            title="BEDROOM"
+            options={["2 BHK", "3 BHK", "4 BHK"]}
+            state={bedroom}
+            setState={setBedroom}
+          />
+        </Col>
+        <Col xs={6} sm={3}>
+          <Filter
+            title="PRICE RANGE"
+            options={["60 - 80 Lakhs", "75 Lakhs - 2 Crore", "2 Crore & Above"]}
+            state={priceRange}
+            setState={setPriceRange}
+          />
+        </Col>
+        <Col xs={6} sm={3}>
+
+          <div className="banner-search">
+            <div className="banner-search-button">SEARCH PROPERTIES</div>
+          </div>
+        </Col>
+      </Row>
+
+
+
     </Container>
   );
 };

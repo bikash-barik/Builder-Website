8e9f6bf1c5a86064336535495c8c2ef93a403fb9
Sayan <sayandey2106@gmail.com>
2022-08-03 22:00:24 +0530

    team page updated

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -14,14 +14,14 @@ export default function Team() {
                         TeamData.map((data,i)=>(
                     <>
                     
-                    <div className='col-lg-4 col-md-12 col-sm-12' style={{padding:"30px"}}>
+                    <div className='col-lg-3 col-md-12 col-sm-12' style={{padding:"30px"}}>
                         <div className='team-img text-center'>
                             <img src={data.image} className='img-fluid'/>
                             <h2 className='white-text' style={{fontSize:"", fontWeight:"500"}}>{data.name}</h2>
                             <h5 style={{fontSize:"24px", fontWeight:"300"}}>[{data.designation}]</h5>
                          </div>   
                     </div>
-                    <div className='col-lg-8 col-md-12 col-sm-12' style={{padding:"20px"}}>
+                    <div className='col-lg-9 col-md-12 col-sm-12' style={{padding:"20px"}}>
                         <div className='team-details p-auto' style={i===1 || i===5 || i===6 ? {padding:"8% 6%"}:{}}>
                             <p className=' p-auto' style={{fontSize:"", fontWeight:"400"}}> 
                            {data.details}

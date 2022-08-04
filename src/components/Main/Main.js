0b6cadfa484b96b0d05c3c979439b17a26063d4f
Tanvi <tanvitomar592@gmail.com>
2022-08-04 17:02:29 +0530

    hovereffect

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useEffect} from "react";
 import "./Main.css"
 import CarouselData from "../../components/data/carousel";
 import OwlCarousel from 'react-owl-carousel';

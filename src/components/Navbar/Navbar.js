35a3aabb981c973ef5167749b3ebec52164ad7da
piyush <piyushvijay20002@gmail.com>
2022-08-04 15:08:14 +0530

    navbar changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -31,42 +31,42 @@ export default function Drawer() {
     <div>
 
     <Navbar collapseOnSelect expand="lg" className='navbar-bg'>
-      <Container fluid style={{ zIndex:"10", backgroundColor: 'white' }}>
+      <Container fluid style={{ zIndex:"10", backgroundColor: 'white',padding:0 }}>
       
-        <NavLink to="/" style={{ paddingLeft: 50, borderWidth: 0, }}>
-          <Image src={Logo} style={{ height: 95, }}/>
+        <NavLink className="logoimg" to="/" style={{ paddingLeft: 27,paddingbottom:0, borderWidth: 0, }}>
+          <img src={Logo} style={{ height: 75, }}/>
         </NavLink>
         {/* <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand> */}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto navtext">
 
-            <Nav.Link className="n-link" style={{ padding: 30, fontSize: 18, color: '#3171B1', fontWeight: 600 }}>
+            <Nav.Link className="n-link" style={{ color: '#3171B1', fontWeight: 600 }}>
 
               <NavLink to="/communities" >
                 COMMUNITIES
               </NavLink>
             </Nav.Link>
 
-            <Nav.Link className="n-link" style={{ padding: 30, fontSize: 18, color: '#3171B1', fontWeight: 600 }}>
+            <Nav.Link className="n-link" style={{  color: '#3171B1', fontWeight: 600 }}>
               <NavLink to="/offers">OFFERS
               </NavLink>
             </Nav.Link>
 
-            <Nav.Link className="n-link" style={{ padding: 30, fontSize: 18, color: '#3171B1', fontWeight: 600 }}>
+            <Nav.Link className="n-link" style={{ color: '#3171B1', fontWeight: 600 }}>
               <NavLink to="/event">
 
                 EVENTS
               </NavLink>
             </Nav.Link>
 
-            <Nav.Link className="n-link" style={{ padding: 30, fontSize: 18, color: '#3171B1', fontWeight: 600 }}>
+            <Nav.Link className="n-link" style={{  color: '#3171B1', fontWeight: 600 }}>
               <NavLink to="/latest-launch">
                 LATEST LAUNCHES
               </NavLink>
             </Nav.Link>
 
-            <Nav.Link className="n-link" style={{ padding: 30, fontSize: 18, color: '#3171B1', fontWeight: 600 }}>
+            <Nav.Link className="n-link" style={{  color: '#3171B1', fontWeight: 600 }}>
               <NavLink to="/team">
 
                 OUR TEAM
@@ -74,18 +74,18 @@ export default function Drawer() {
             </Nav.Link>
            
           </Nav>
-          <Nav>
-            <Nav.Link className='d-flex right-link navbtn' >
+          <Nav className="rightnav">
+            <Nav.Link className=' right-link navbtn' >
               {/* <i class="fa fa-video mx-2  my-1"></i> */}
               <i class="bi bi-camera-video iconbi"></i>
               <p className="textp" >Instant Video Call</p>
             </Nav.Link>
-            <Nav.Link className='d-flex right-link navbtn' >
+            <Nav.Link className=' right-link navbtn' >
               {/* <i class="fa-brands fa-whatsapp mx-2  my-1"></i> */}
               <i class="bi bi-whatsapp iconbi"></i>
               <p className="textp" >Whatsapp</p>
             </Nav.Link>
-            <Nav.Link className='d-flex right-link getbtn' style={{paddingTop:"10px"}}>
+            <Nav.Link className=' right-link getbtn' style={{}}>
               <button className='btn btn-primary ' onClick={()=>{
                 modal===true ? setModal(false):setModal(true)
 

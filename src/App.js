c13fe29677d9f923a28760013b5f6ecd13c9b6e4
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-08-04 23:05:26 +0530

    Offers page edited

--- src/App.js
+++ src/App.js
@@ -1,55 +1,93 @@
-import { useEffect } from 'react';
-import './App.css';
-import Event from './pages/Event/Event';
-import {
-  BrowserRouter ,
-  Route,
-  Routes
-} from "react-router-dom";
-import Communities from './pages/Communities/Communities.js';
-import Team from './components/Team/Team';
-import Ongoing from './components/Communities/Ongoing/Ongoing';
-import AOS from 'aos';
-import SingleProperty from './components/SingleProperty/SingleProperty';
-import Slider2 from './components/SingleProperty/Slider2';
-import OngoingData from './components/data/ongoing';
-import Drawer from './components/Navbar/Navbar'
-import Offer from './components/offers/Offers'
-import Landing from "./pages/Landing"
+import { useEffect } from "react";
+import "./App.css";
+import Event from "./pages/Event/Event";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Communities from "./pages/Communities/Communities.js";
+import Team from "./components/Team/Team";
+import Ongoing from "./components/Communities/Ongoing/Ongoing";
+import AOS from "aos";
+import SingleProperty from "./components/SingleProperty/SingleProperty";
+import Slider2 from "./components/SingleProperty/Slider2";
+import OngoingData from "./components/data/ongoing";
+import Drawer from "./components/Navbar/Navbar";
+import Offer from "./components/offers/Offers";
+import Landing from "./pages/Landing";
 
-import Footer from './components/footer';
+import Footer from "./components/footer";
 
 function App() {
-
-useEffect(() => {
-  AOS.init()
-}, [])
-
-
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
   return (
-  
-    
-<BrowserRouter>
+    <BrowserRouter>
+      <Drawer />
 
-  <Drawer/>
- 
-        <Routes>
-        <Route exact path="/" element={<Landing/>}/>
-         <Route exact path="/communities" element={<div><Communities/></div>}/>
-         <Route path ="/event" element={<div><Event/></div>}/>
-         <Route path ="/team" element={<div><Team/></div>}/>
-         <Route path ="/latest-launch" element={<div><Ongoing head="LATEST ONGOING PROJECTS"/></div>}/>
-         <Route path ="/offers" element={<div><Offer/></div>}/>
-         <Route path ="/communities/:id" element={<div><SingleProperty OngoingData={OngoingData}/></div>}/>
-         <Route exact path="/slider" element={<div><Slider2/></div>}/>
-         </Routes>
-    <Footer/>
-</BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route
+          exact
+          path="/communities"
+          element={
+            <div>
+              <Communities />
+            </div>
+          }
+        />
+        <Route
+          path="/event"
+          element={
+            <div>
+              <Event />
+            </div>
+          }
+        />
+        <Route
+          path="/team"
+          element={
+            <div>
+              <Team />
+            </div>
+          }
+        />
+        <Route
+          path="/latest-launch"
+          element={
+            <div>
+              <Ongoing head="LATEST ONGOING PROJECTS" />
+            </div>
+          }
+        />
+        <Route
+          path="/offers"
+          element={
+            <div>
+              <Offer />
+            </div>
+          }
+        />
+        <Route
+          path="/communities/:id"
+          element={
+            <div>
+              <SingleProperty OngoingData={OngoingData} />
+            </div>
+          }
+        />
+        <Route
+          exact
+          path="/slider"
+          element={
+            <div>
+              <Slider2 />
+            </div>
+          }
+        />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
-       
-
-  
 }
 
 export default App;

5202f3606d4a951d341a84d55716786f3363935c
Aditya <singhkaditya22@gmail.com>
2022-08-06 04:18:37 +0530

    description , detail and ameneties subcomponents added

--- src/components/SingleProperty/SingleProperty.js
+++ src/components/SingleProperty/SingleProperty.js
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import "./SingleProperty.css";
 import gallery from "../../images/ongoing/gallery.png";
 import share from "../../images/ongoing/share2.png";
 import { Link } from "react-router-dom";
-import facilities from "../../images/ongoing/archid.png";
 import Modal from "react-modal";
-import { Button } from "react-bootstrap";
 import FloorPlan from "./subcomponent/onGoingSiteFloorPlans/FloorPlan";
 import HighLights from "./subcomponent/Highlights";
+import Description from "./subcomponent/Description";
+import Details from "./subcomponent/Details";
+import Ameneties from "./subcomponent/ConstructionUpdates/Ameneties";
 import {
   ranasinghpur,
   patrapada,
   oldTown,
 } from "./subcomponent/onGoingSiteFloorPlans/imageConfig";
-// import OngoingData from '../data/ongoing';
+
 
 export default function SingleProperty({ OngoingData }) {
   const { id } = useParams();
@@ -32,18 +33,17 @@ export default function SingleProperty({ OngoingData }) {
       zIndex: "200",
     },
   };
-  console.log(id);
+  
   const [paramId, setparamid] = useState(id);
 
   return (
     <div className="single_background">
-      {OngoingData.filter((list) => list.id === paramId).map((data, i) => (
-        
-          <div>
-<<<<<<< HEAD
+      {OngoingData.filter((list) => list.id === paramId).map((data, i) => {
+        return(
+        <div key={i} >
             <div
               className="landing-image"
-              style={{ backgroundImage: `url(${data?.bg})` }}
+              style={{ backgroundImage: `url(${data.bg})` }}
             >
               <div className="single-property-button-group">
                 <div
@@ -52,7 +52,7 @@ export default function SingleProperty({ OngoingData }) {
                     setModal(true);
                   }}
                 >
-                  REGISTER YOUR INTEREST{" "}
+                  REGISTER YOUR INTEREST
                 </div>
                 <Link to={`/slider`}>
                   <div className="single-property-button">
@@ -68,331 +68,16 @@ export default function SingleProperty({ OngoingData }) {
                     src={share}
                     alt="Loading..."
                   />
-=======
-            <p></p>
-            <div className="landing-image">
-              <img src={data.bg} />
-            </div>
-            <div className="button-grp">
-              <div
-                className="btn btn-primary mx-2 px-2"
-                onClick={() => {
-                  setModal(true);
-                }}
-              >
-                REGISTER YOUR INTEREST{" "}
-              </div>
-              <Link to={`/slider`}>
-                <div className="btn btn-light mx-2 ">
-                  GALLERY{" "}
-                  <img
-                    src={gallery}
-                    style={{ width: "1.3rem", marginLeft: "6px" }}
-                  />{" "}
->>>>>>> bbc61891342a5ed29d43da9301808ff052ebdabe
                 </div>
               </div>
             </div>
-<<<<<<< HEAD
-            <HighLights/>
-  
+            <HighLights highlightImages={data.highlights} />
+            <Description title={data.title} description={data.description} />
+            <Details detail={data.details} />
+            <Ameneties ameneties={data.ameneties} />
           </div>
         
-=======
-            <section className="highlights container-fluid text-center ">
-              <div className="d-flex">
-                <h5>Project highlights </h5>
-                <hr
-                  style={{
-                    width: "150px",
-                    backgroundColor: "white",
-                    fontWeight: "",
-                    height: "1.3px",
-                    marginLeft: "5px",
-                    opacity: "0.8",
-                  }}
-                />
-              </div>
-              <div className="row my-2 justify-content-center">
-                {data.highlight.map((img, i) => (
-                  <div className="col-lg-3 col-md-3 col-sm-4 my-2">
-                    <img
-                      src={img.detail}
-                      className="detail-img"
-                      data-aos={i <= 4 ? "fade-right" : "fade-left"}
-                      data-aos-easing="linear"
-                      data-aos-duration="1500"
-                    ></img>
-                  </div>
-                ))}
-              </div>
-              <div className="detail-1">
-                <div className="d-flex">
-                  <h4 className="black-text" style={{ width: "350px" }}>{data.title}</h4>
-                  <hr
-                    style={{
-                      width: "250px",
-                      backgroundColor: "black",
-                      fontWeight: "",
-                      height: "1.3px",
-                      marginLeft: "5px",
-                      opacity: "0.8",
-                    }}
-                  />
-                </div>
-                <div className="black-text">
-                  <h5 className="black-text my-4">{data.details_1}</h5>
-                </div>
-              </div>
-              <div className=" container-fluid">
-                <div className="row">
-                  <div className="col-lg-6 col-md-6 col-sm-12 p-0">
-                    <div className="detail-2">
-                      <div className="d-flex">
-                        <h5 className="black-text" style={{width:"400px"}}>
-                          {data.heading_2}
-                        </h5>
-                        <hr
-                          style={{
-                            width: "200px",
-                            backgroundColor: "black",
-                            fontWeight: "",
-                            height: "1.3px",
-                            marginLeft: "5px",
-                            opacity: "0.8",
-                          }}
-                        />
-                      </div>
-                      <p
-                        className="black-text"
-                        data-aos="fade-up"
-                        data-aos-easing="linear"
-                        data-aos-duration="1500"
-                      >
-                        {data.details_2}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-lg-6 col-md-6 col-sm-12 p-0">
-                    <img
-                      data-aos="fade-down-left"
-                      data-aos-easing="linear"
-                      data-aos-duration="1500"
-                      src={data.detailImage1}
-                      style={{ height: "100%", width: "100%" }}
-                      className="img-fluid"
-                    ></img>
-                  </div>
-                </div>
-              </div>
-
-              <div className="container-fluid my-5">
-                <div className="row">
-                  <div className="col-lg-6 col-md-6 col-sm-12 p-0">
-                    <img
-                      data-aos="fade-up-right"
-                      data-aos-easing="linear"
-                      data-aos-duration="1500"
-                      src={data.detailImage2}
-                      style={{ height: "100%", width: "100%" }}
-                      className="img-fluid"
-                    ></img>
-                  </div>
-                  <div className="col-lg-6 col-md-6 col-sm-12 p-0">
-                    <div className="detail-3">
-                      <div className="d-flex">
-                        <h5 className="black-text" style={{width:"400px"}} >
-                        {data.heading_3} 
-                        </h5>
-                        <hr
-                          style={{
-                            width: "200px",
-                            backgroundColor: "black",
-                            fontWeight: "",
-                            height: "1.3px",
-                            marginLeft: "5px",
-                            opacity: "0.8",
-                          }}
-                        />
-                      </div>
-                      <p
-                        className="black-text"
-                        data-aos="fade-up"
-                        data-aos-easing="linear"
-                        data-aos-duration="1500"
-                      >
-                        {data.details_3}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className=" container-fluid">
-                <div className="row">
-                  <div className="col-lg-6 col-md-6 col-sm-12 p-0">
-                    <div className="detail-2">
-                      <div className="d-flex">
-                        <h5 className="black-text" style={{width:"400px"}} >
-                           {data.heading_4}
-                        </h5>
-                        <hr
-                          style={{
-                            width: "200px",
-                            backgroundColor: "black",
-                            fontWeight: "",
-                            height: "1.3px",
-                            marginLeft: "5px",
-                            opacity: "0.8",
-                          }}
-                        />
-                      </div>
-                      <p
-                        className="black-text"
-                        data-aos="fade-up"
-                        data-aos-easing="linear"
-                        data-aos-duration="1500"
-                      >
-                        {data.details_4}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-lg-6 col-md-6 col-sm-12 p-0">
-                    <img
-                      data-aos="fade-down-left"
-                      data-aos-easing="linear"
-                      data-aos-duration="1500"
-                      data-aos-offset="400"
-                      src={data.detailImage3}
-                      style={{ height: "100%", width: "100%" }}
-                      className="img-fluid"
-                    ></img>
-                  </div>
-                </div>
-              </div>
-
-              <div className=" container-fluid my-5">
-                <div className="row">
-                  <div className="col-lg-6 col-md-6 col-sm-12 p-0">
-                    <div className="detail-2">
-                      <div className="d-flex">
-                        <h5 className="black-text">AMENETIES </h5>
-                        <hr
-                          style={{
-                            width: "200px",
-                            backgroundColor: "black",
-                            fontWeight: "",
-                            height: "1.3px",
-                            marginLeft: "5px",
-                            opacity: "0.8",
-                          }}
-                        />
-                      </div>
-                      <p
-                        className="black-text"
-                        data-aos="fade-up"
-                        data-aos-easing="linear"
-                        data-aos-duration="1500"
-                      >
-                        {data.amenities}
-                      </p>
-                      <a href={data.brochure} target="blank">
-                        <div
-                          className="btn btn-primary"
-                          style={{
-                            maxWidth: "400px",
-                            width: "100%",
-                            fontSize: "20px",
-                            backgroundColor: "grey",
-                          }}
-                        >
-                          Download Brochure
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                  <div className="col-lg-6 col-md-6 col-sm-12 p-0">
-                    <img
-                      src={facilities}
-                      style={{ height: "600px", width: "100%" }}
-                      className="img-fluid"
-                    ></img>
-                  </div>
-                </div>
-              </div>
-              {id === "1" && <FloorPlan floorPlanData={patrapada} />}
-              {id === "2" && <FloorPlan floorPlanData={ranasinghpur} />}
-              {id === "3" && <FloorPlan floorPlanData={oldTown} />}
-
-              <div
-                className=" container-fluid my-5"
-                style={{ backgroundColor: "azure" }}
-              >
-                <div>
-                  <div className="d-flex">
-                    <h3 className="black-text">Finance facilities </h3>
-                    <hr
-                      style={{
-                        width: "150px",
-                        backgroundColor: "black",
-                        fontWeight: "",
-                        height: "1.3px",
-                        marginLeft: "5px",
-                        opacity: "0.8",
-                      }}
-                    />
-                  </div>
-                  <h3 className="black-text">
-                    We are approved by all leading banks like-
-                  </h3>
-                </div>
-                <div className="row">
-                  {/* <hr
-                          style={{
-                            width: "150px",
-                            backgroundColor: "black",
-                            fontWeight: "",
-                            height: "1.3px",
-                            marginLeft: "5px",
-                            opacity: "0.8",
-                          }}
-                        /> */}
-                  {data.finance.map((img) => (
-                    <div className="col-lg-3 col-md-3 col-sm-6 p-0">
-                      <img
-                        src={img.image}
-                        style={{
-                          height: "100px",
-                          width: "200px",
-                          padding: "10px",
-                        }}
-                        className="black-text  img-fluid"
-                        data-aos="fade-up"
-                        data-aos-easing="linear"
-                        data-aos-duration="1500"
-                      ></img>
-                    </div>
-                  ))}
-                </div>
-              </div>
-
-              <div className="container-fluid my-5">
-                <div className="row">
-                  {data.location.map((img) => (
-                    <div className="col-lg-6 col-md- col-sm-12 p-0">
-                      <img
-                        src={img.image}
-                        style={{ width: "100%", height: "500px" }}
-                      ></img>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </section>
-          </div>
-        </>
->>>>>>> bbc61891342a5ed29d43da9301808ff052ebdabe
-      ))}
+      )})}
 
       <Modal isOpen={modal} style={customStyles} contentLabel="Example Modal">
         <form className="">
@@ -447,7 +132,6 @@ export default function SingleProperty({ OngoingData }) {
     </div>
   );
 }
-<<<<<<< HEAD
 
 // <section className="highlights container-fluid text-center ">
 //   <div className="d-flex">
@@ -734,5 +418,3 @@ export default function SingleProperty({ OngoingData }) {
 //     </div>
 //   </div>
 // </section>
-=======
->>>>>>> bbc61891342a5ed29d43da9301808ff052ebdabe
